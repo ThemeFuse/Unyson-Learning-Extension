3abafeb7fb003199a81e99c38b0d9c94846d5dfa
moldcraft <moldcraft@email.com>
2014-12-01 12:24:06 +0200

    Updated manifest version
    to prevent infinite "New update avaialble"

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Learning', 'fw' );
 $manifest['description'] = __( 'This extension adds a Learning module to your theme. Using this extension you can add courses, lessons and tests for your users to take.', 'fw' );
-$manifest['version']     = '1.0.0';
+$manifest['version']     = '1.0.2';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
