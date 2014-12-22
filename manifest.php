c84f44b96b258fa41ae38168a0da0dac9cebced7
moldcraft <moldcraft@email.com>
2014-12-22 14:20:51 +0200

    Version 1.0.4

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Learning', 'fw' );
 $manifest['description'] = __( 'This extension adds a Learning module to your theme. Using this extension you can add courses, lessons and tests for your users to take.', 'fw' );
-$manifest['version']     = '1.0.3';
+$manifest['version']     = '1.0.4';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
